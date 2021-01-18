0bb98bb768875165364a58eca4621c7c36478ad6
UmaCS <wiutumka@gmail.com>
2021-01-17 16:15:25 -0800

    last commit

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,7 @@ class Game extends Component {
       dice: Array.from({ length: NUM_DICE }),
       locked: Array(NUM_DICE).fill(false),
       rollsLeft: NUM_ROLLS,
+      rolling: false,
       scores: {
         ones: undefined,
         twos: undefined,
@@ -32,6 +33,15 @@ class Game extends Component {
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
     this.toggleLocked = this.toggleLocked.bind(this);
+    this.animateRolling = this.animateRolling.bind(this);
+  }
+  componentDidMount(){
+    this.animateRolling();
+  }
+  animateRolling(){
+    this.setState({rolling: true}, () => {
+      setTimeout(this.roll, 1000)
+    });
   }
 
   roll(evt) {
@@ -41,19 +51,23 @@ class Game extends Component {
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
-      rollsLeft: st.rollsLeft - 1
+      rollsLeft: st.rollsLeft - 1,
+      rolling: false
     }));
   }
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
-    this.setState(st => ({
+    if (this.state.rollsLeft > 0 && !this.state.rolling) {
+      this.setState(st => ({
       locked: [
         ...st.locked.slice(0, idx),
        (st.rollsLeft > 0 ? !st.locked[idx] : st.locked[idx]),
         ...st.locked.slice(idx + 1)
       ]
-    }));
+      }));
+    }
+    
   }
 
   doScore(rulename, ruleFn) {
@@ -63,10 +77,22 @@ class Game extends Component {
       rollsLeft: NUM_ROLLS,
       locked: Array(NUM_DICE).fill(false)
     }));
-    this.roll();
+    this.animateRolling();
+  }
+
+  displayRollInfo(){
+    // {this.state.rollsLeft}
+    const messages = [
+      '0 Rolls left',
+      '1 Rolls left',
+      '2 Rolls left',
+      'Starting Round'
+    ];
+    return messages[this.state.rollsLeft];
   }
 
   render() {
+    const {dice, locked, rollsLeft, rolling, scores} = this.state;
     return (
       <div className='Game'>
         <header className='Game-header'>
@@ -74,23 +100,28 @@ class Game extends Component {
 
           <section className='Game-dice-section'>
             <Dice
-              dice={this.state.dice}
-              locked={this.state.locked}
-              handleClick={this.toggleLocked}
-              disabled={this.state.rollsLeft === 0}
+                locked={locked}
+                handleClick={this.toggleLocked}
+                dice={dice}
+                disabled={rollsLeft === 0}
+                rolling={rolling}
             />
             <div className='Game-button-wrapper'>
               <button
                 className='Game-reroll'
-                disabled={this.state.locked.every(x => x)}
-                onClick={this.roll}
+                disabled={
+                          locked.every(x => x) || 
+                          rollsLeft === 0 || 
+                          rolling
+                        }
+                onClick={this.animateRolling}
               >
-                {this.state.rollsLeft} Rerolls Left
+                {this.displayRollInfo()}
               </button>
             </div>
           </section>
         </header>
-        <ScoreTable doScore={this.doScore} scores={this.state.scores} />
+        <ScoreTable doScore={this.doScore} scores={scores} />
       </div>
     );
   }
